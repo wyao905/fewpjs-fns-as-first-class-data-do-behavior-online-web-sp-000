00e20f6e5e622b82c96f7537b5320977664b9e42
Warren Yao <warrenyca905@gmail.com>
2020-01-14 22:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
-function greet(time){
-  
+function greet(timeString){
+  let timeArray = timeString.split(":");
+  let timeI = parseInt(timeArray[0]);
+  if()
 }
